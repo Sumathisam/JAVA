4d3293ed970b96a22eeb9327e57424abc11067f0
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-04-19 20:43:04 +0530

    Add files via upload

---
+++ practical/practial/IfElseExample.java
@@ -0,0 +1,16 @@
+import java.until.scanner; //import scanner class
+class IfElseExample{
+public void display(){
+// int age = 30;
+scanner s = new scanner(Systeam.int);// create a scanner object
+Systeam.out.prinatln('Enter age');
+int age = s.nextlnt(); //read user inpit
+
+if(age >=18) {
+//true
+Systeam.out.println('You are eligible");
+}
+else{
+//false
+Systeam.oit.println('you are not eligible');
+}
