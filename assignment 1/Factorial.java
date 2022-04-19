a84790149ee4f5d6c087c4edd2076f7191e0fba3
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-04-19 20:35:44 +0530

    Add files via upload

---
+++ assignment 1/Factorial.java
@@ -0,0 +1,10 @@
+Public Class Factorial {
+public Static void main(String[]args){
+int n =7;
+int result=1;
+for (int i=1;<n:i++){
+result =result*i;
+}
+Systeam.out.println("The factorial of"+n+"is"+result);
+}
+}
