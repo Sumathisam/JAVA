9b89c094fd19ecadff62bbbd595bb80552850569
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-04-19 20:45:10 +0530

    Add files via upload

---
+++ practical/practial/SampleMetalExample.java
@@ -0,0 +1,14 @@
+Class Metal{
+String Weight = "95";
+String colour = "silver";
+}
+Class Elements extends Metal{
+String modelname = "hue" ;
+}
+class SampleMetalExample{
+public satic void main(String[] args){
+Elements e = new Elements();
+System.out.println("Elements modelname is : " + e.modelname
+}
+}
+
