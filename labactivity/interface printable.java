58410797708a32461844a70fc4d23fa95bd659e9
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-04-21 14:34:48 +0530

    Add files via upload

---
+++ labactivity/interface printable.java
@@ -0,0 +1,16 @@
+interface printable{
+  void print();
+ }
+ interface showble{
+    void show();
+ }
+ class MultipleInheritence implements
+ public void print(){Systeam.out.println
+ public void show(){Systeam.out.println
+ 
+ public static void main(String args[]
+ MultipleInheritence obj = new MultipleInherite
+ obj.print();
+ obj.show();
+ }
+ }
