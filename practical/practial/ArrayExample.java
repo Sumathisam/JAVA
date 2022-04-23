6f23bb2794f5e6928b8f577d07d4aa67cbddf585
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-04-23 14:14:32 +0530

    Add files via upload

---
+++ practical/practial/ArrayExample.java
@@ -0,0 +1,15 @@
+class ArrayExample{
+public static void main(string args[])
+{
+string name={" Mamta "," Ram "," Aajit "," Tushar "};
+//Systeam.out.println(names[1];
+for (int i=0; i<length;i++){
+Systeam.out.println(names[i]);
+}
+Systeam.out.println();
+int []phone = {123,345,567,789,955,533,555,111,1906,3456};
+for (int j = 0; j<length;j++){
+Systeam.out.println(name);
+}
+}
+}
