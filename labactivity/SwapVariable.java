a8aecbc0d2266bd65b794ad7307d824683ef554d
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-05-07 14:20:00 +0530

    Add files via upload

---
+++ labactivity/SwapVariable.java
@@ -0,0 +1,12 @@
+ class SwapVariable {
+ public static void main(String[] args) {
+ int a=20;
+ int b=30;
+ System.out.println("a="+a);
+ System.out.println("b="+b);
+ a=a+b;
+ b=a-b;
+ System.out.println("a="+a);
+ System.out,println("b="+b);
+ }
+ }
