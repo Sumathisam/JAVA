678a7e94b2fc9381831ad89b681531aab0a7f0d0
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-05-24 18:03:24 +0530

    Add files via upload

---
+++ assignment 1/SubString.java
@@ -0,0 +1,10 @@
+public class subString {
+public static void main(String[]args) { 
+String str = "the quick brown fox jumps over the lazy dog.";
+
+String new_str = str.substring(10,26);
+
+System.out.println(old = " + str);
+System.out.println(new = " + new_str);
+}
+}
