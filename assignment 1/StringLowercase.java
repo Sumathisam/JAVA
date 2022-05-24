678faef0878b16bd87ebcbbba7f4ac95ef4cad3f
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-05-24 13:44:28 +0530

    Add files via upload

---
+++ assignment 1/StringLowercase.java
@@ -0,0 +1,8 @@
+public class stringLowercase{
+public static void main (String[] args) {
+String str = "the quit Brown FoX!";
+String lowerstr = toLowerCase();
+System.out.println("original String: " str);
+Systeam.out.println("String in Lowercase: " + lowerstr);
+}
+}
