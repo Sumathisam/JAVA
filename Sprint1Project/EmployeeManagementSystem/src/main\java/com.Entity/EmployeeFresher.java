a50aab97a10ba7a4d05aa3c184a413532a6eff20
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-08-06 12:20:38 +0530

    Create EmployeeFresher.java

---
+++ Sprint1Project/EmployeeManagementSystem/src/main\java/com.Entity/EmployeeFresher.java
@@ -0,0 +1,48 @@
+package com.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="employee_Fresherdetails")
+@Entity
+
+public class EmployeeFresher {
+	@Id
+	private int EmpId;
+	private String Name;
+	private String Location;
+	private long PhoneNo;
+	private String EmpTitle;
+	public int getEmpId() {
+		return EmpId;
+	}
+	public void setEmpId(int empId) {
+		EmpId = empId;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	public long getPhoneNo() {
+		return PhoneNo;
+	}
+	public void setPhoneNo(long phoneNo) {
+		PhoneNo = phoneNo;
+	}
+	public String getEmpTitle() {
+		return EmpTitle;
+	}
+	public void setEmpTitle(String empTitle) {
+		EmpTitle = empTitle;
+	}
+
+}
