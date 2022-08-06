e73f6e91c5d4b4074dacc5953f3fca4f5beb5d55
Sumathisam <102944949+Sumathisam@users.noreply.github.com>
2022-08-06 12:23:07 +0530

    Create TestBuild.java

---
+++ Sprint1Project/EmployeeManagementSystem/src/main\java/com/TestBuild.java
@@ -0,0 +1,90 @@
+package com;
+
+import java.util.Scanner;
+
+import com.DaoImpl.EmpFreshImpl;
+import com.DaoImpl.EmpSalImpl;
+
+public class TestBuild {
+	public static void main(String[] args) {
+		EmpFreshImpl ef = new EmpFreshImpl();
+		EmpSalImpl es = new EmpSalImpl();
+		
+		while (true) {  
+			
+			Scanner s = new Scanner(System.in);
+			System.out.println("-------------Employee Management System-------------");
+			System.out.println("Enter Choice");
+			System.out.println(" 1. Manage Employee Details \n 2. Manage Salary Details \n 3. Exit");
+			int ch = s.nextInt();
+			switch (ch) {
+			case 1:
+				Scanner s1 = new Scanner(System.in);
+				System.out.println("-------------Manage Employee Details-------------");
+				System.out.println("Enter Choice");
+				System.out.println(" 1. Create Employee Details \n 2. Update Employee Details \n 3. Delete Employee Details \n 4. Show Employee Details \n 5. Exit");
+				int ch1 = s.nextInt();
+				switch (ch1) {
+				case 1:
+					ef.CreateEmp();	
+				break;
+				case 2:
+					ef.UpdateEmp();
+				break;
+				case 3:
+					ef.DeleteEmp();
+				break;
+				case 4:
+					ef.ShowEmp();
+				break;	
+				case 5:
+					System.out.println("Back to menu");
+				default:
+					System.out.println("Wrong Choice");
+					
+				}
+				
+					
+				
+				break;
+				
+            case 2:
+            	Scanner s2 = new Scanner(System.in);
+            	System.out.println("-------------Manage Employee Details-------------");
+            	System.out.println("Enter Choice");
+            	System.out.println(" 1. Create Salary Details \n 2. Update Salary Details \n 3. Delete Salary Details \n 4. Show Salary Details \n 5. Exit");
+            	int ch2 = s.nextInt();
+            	switch (ch2) {
+            	case 1:
+            		es.CreateSal();
+            		break;
+            	case 2:
+            		es.UpdateSal();
+            		break;
+            	case 3:
+            		es.DeleteSal();
+            		break;
+            	case 4:
+            		es.ShowSal();
+            		break;
+            	}
+
+             break;
+				
+            case 3:
+            	System.out.println("Thank You");
+            	System.exit(0);
+				
+				break;
+
+			default:
+				System.out.println("Wrong Choice");
+				
+				break;
+				
+			}
+			
+		}
+	}
+
+}
